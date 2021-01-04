0e0abea398d8ea65bef85b0bdac9395334a235ff
DiazMauro <diaz.mauro.e@gmail.com>
2021-01-04 15:58:24 -0300

    modifique varias cosas, subo el register

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.resolve(__dirname, './Site');
+
+app.use (express.static(publicPath));
+
+app.get ('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./Site/SRC/home.html'))
+})
+app.get ('/register', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./Site/SRC/register.html'))
+})
+
+
+
+app.listen (3000, () => 
+    console.log('servidor corriendo!!!')
+);
+
+
