783c77d2e2874ac6dd8a43f39f5b8a0b451f4f24
DiazMauro <diaz.mauro.e@gmail.com>
2021-01-07 20:33:40 -0300

    modificando para ver lo de luis

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get ('/register', (req, res) =>{
     res.sendFile(path.resolve(__dirname,'./Site/SRC/register.html'))
 })
 
+app.get ('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./Site/SRC/login.html'))
+})
+
 
 
 app.listen (3000, () => 
