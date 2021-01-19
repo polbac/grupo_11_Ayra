46aacb5d90223e3654fc5d59c85876114d44f58e
polbac <pablob@projectcor.com>
2021-01-19 14:45:46 -0300

    move view

--- Site/SRC/app.js
+++ Site/SRC/app.js
@@ -4,30 +4,12 @@ const app = express();
 const publicPath = path.resolve(__dirname);
 const mainRouter = require('./routes/mainRouter');
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', './src/views');
 
 app.use (express.static(publicPath));
 
 app.use('/', mainRouter);
-/* 
-app.get ('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./Site/SRC/home.html'))
-})
-app.get ('/register.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./Site/SRC/register.html'))
-})
 
-app.get ('/login.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./Site/SRC/login.html'))
-})
-
-app.get ('/shoppingcart.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./Site/SRC/shoppingcart.html'))
-})
-
-app.get ('/prueba.html', function(req, res){
-    res.send (series)
-}) */
 
 app.listen (3000, () =>
     console.log('servidor corriendo en 3000!!!')
